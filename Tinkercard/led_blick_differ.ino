046e8a8ba4df4922215f94bbae4f2d2b0ba2c84b
Denys Shabelnyk <dionisiy1986@gmail.com>
2024-11-02 11:23:46 +0100

    debug

---
+++ Tinkercard/led_blick_differ.ino
@@ -0,0 +1,15 @@
+// C++ code
+void setup() {
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+  Serial.println("LED ON/OFF");
+}
+
+void loop() {
+  Serial.println("LED is ON");
+  digitalWrite(13, HIGH);
+  delay(2000);
+  Serial.println("LED is OFF");
+  digitalWrite(13, LOW);
+  delay(1000);
+}
