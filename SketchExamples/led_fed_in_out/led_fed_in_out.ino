fbbbd24aa67a24b2b2122c1314169cb48cabbe35
Denys Shabelnyk <dionisiy1986@gmail.com>
2024-11-23 14:56:05 +0100

    final scetch

--- SketchExamples/led_fed_in_out/led_fed_in_out.ino
+++ SketchExamples/led_fed_in_out/led_fed_in_out.ino
@@ -6,18 +6,18 @@ void setup() {
   pinMode(LED_PIN,OUTPUT);
   analogWrite(LED_PIN,PWD_MIN);
 }
-// not complite
+
 void loop() {
-  // fed in
-  for(int i = PWD_MIN;i <= PWD_MAX;i++) {
+    
+  for(int i=PWD_MIN,j=PWD_MAX;i<=PWD_MAX,j>=PWD_MIN;i++,j--) 
+  {
     analogWrite(LED_PIN,i);
-    delay(500);
-  }
-  delay(5000);
-  analogWrite(LED_PIN,PWD_MAX);
-  // fed out
-  for(int i = PWD_MAX;i >= PWD_MIN;i--) {
-    analogWrite(LED_PIN,i);
-    delay(500);
-  }
+    delay(1000);
+    analogWrite(LED_PIN,j);
+    delay(1000);
+   }
 }
+
+
+
+
