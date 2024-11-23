c76958bd4c6b4d44bd89b852ede78f0496d94782
Denys Shabelnyk <dionisiy1986@gmail.com>
2024-11-23 15:09:48 +0100

    LED final

--- SketchExamples/led_fed_in_out/led_fed_in_out.ino
+++ SketchExamples/led_fed_in_out/led_fed_in_out.ino
@@ -11,9 +11,9 @@ void loop() {
   for(int i=PWD_MIN,j=PWD_MAX;i<=PWD_MAX,j>=PWD_MIN;i++,j--) 
   {
     analogWrite(LED_PIN,i);
-    delay(1000);
+    delay(2000);
     analogWrite(LED_PIN,j);
-    delay(1000);
+    delay(2000);
   }
 }
 
